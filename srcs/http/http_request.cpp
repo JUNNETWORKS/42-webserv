8bdc94579ac3c5561fa6ea0f1acaab1ded246c01
Junichi Sasaki <junichi200123@gmail.com>
2022-05-18 07:47:54 +0900

    HttpRequestのコンストラクタなどを実装｡HttpRequest.AddDataToBuffer()を実装｡

---
+++ srcs/http/http_request.cpp
@@ -0,0 +1,33 @@
+#include "http/http_request.hpp"
+
+#include <stdio.h>
+
+namespace http {
+
+HttpRequest::HttpRequest() {
+  this->buffer_.reserve(reserve_size_);
+}
+
+HttpRequest::HttpRequest(const HttpRequest &rhs) {
+  *this = rhs;
+}
+
+HttpRequest &HttpRequest::operator=(const HttpRequest &rhs) {
+  if (this != &rhs) {
+    method_ = rhs.method_;
+    path_ = rhs.path_;
+    headers_ = rhs.headers_;
+    body_ = rhs.body_;
+    buffer_ = rhs.buffer_;
+  }
+  return *this;
+}
+
+HttpRequest::~HttpRequest() {}
+
+void HttpRequest::AddDataToBuffer(HttpRequest::Byte *buf, size_t size) {
+  buffer_.insert(buffer_.end(), buf, buf + size);
+  printf("current buf len: %lu", buffer_.size());
+}
+
+};  // namespace http
