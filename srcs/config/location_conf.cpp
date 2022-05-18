e8ac22e7e29cf1656a250e0dcccad97b8d68b1b0
Junichi Sasaki <junichi200123@gmail.com>
2022-05-18 17:53:47 +0900

    LocationConf のメソッド実装

--- srcs/config/location_conf.cpp
+++ srcs/config/location_conf.cpp
@@ -1,3 +1,122 @@
-#include "config/config.hpp"
+#include "config/location_conf.hpp"
 
-namespace config {};
+#include "utils/string.hpp"
+
+namespace config {
+
+LocationConf::LocationConf() {}
+
+LocationConf::LocationConf(const LocationConf &rhs) {
+  *this = rhs;
+}
+
+LocationConf &LocationConf::operator=(const LocationConf &rhs) {
+  if (this != &rhs) {
+    path_pattern_ = rhs.path_pattern_;
+    is_backward_search_ = rhs.is_backward_search_;
+    allowed_methods_ = rhs.allowed_methods_;
+    client_max_body_size_kb_ = rhs.client_max_body_size_kb_;
+    root_dir_ = rhs.root_dir_;
+    index_pages_ = rhs.index_pages_;
+    is_cgi_ = rhs.is_cgi_;
+    error_pages_ = rhs.error_pages_;
+    auto_index_ = rhs.auto_index_;
+    redirect_url_ = rhs.redirect_url_;
+  }
+  return *this;
+}
+
+LocationConf::~LocationConf() {}
+
+std::string LocationConf::GetPathPattern() {
+  return path_pattern_;
+}
+
+void LocationConf::SetPathPattern(std::string path_pattern) {
+  path_pattern_ = path_pattern;
+}
+
+bool LocationConf::GetIsBackwardSearch() {
+  return is_backward_search_;
+}
+
+void LocationConf::SetIsBackwardSearch(bool is_backward_search) {
+  is_backward_search_ = is_backward_search;
+}
+
+bool LocationConf::IsMethodAllowed(std::string method) {
+  return allowed_methods_.find(method) != allowed_methods_.end();
+}
+
+void LocationConf::AppendAllowedMethod(std::string method) {
+  allowed_methods_.insert(method);
+}
+
+int64_t LocationConf::GetClientMaxBodySizeKB() {
+  return client_max_body_size_kb_;
+}
+
+void LocationConf::SetClientMaxBodySizeKB(int64_t client_max_body_size_kb) {
+  client_max_body_size_kb_ = client_max_body_size_kb;
+}
+
+std::string LocationConf::GetRootDir() {
+  return root_dir_;
+}
+
+void LocationConf::SetRootDir(std::string root_dir) {
+  root_dir_ = root_dir;
+}
+
+const std::vector<std::string> &LocationConf::GetIndexPages() {
+  return index_pages_;
+}
+
+void LocationConf::AppendIndexPages(std::string filepath) {
+  index_pages_.push_back(filepath);
+}
+
+bool LocationConf::GetIsCgi() {
+  return is_cgi_;
+}
+
+void LocationConf::SetIsCgi(bool is_cgi) {
+  is_cgi_ = is_cgi;
+}
+
+const std::map<http::HttpStatus, std::string> &LocationConf::GetErrorPages() {
+  return error_pages_;
+}
+
+void LocationConf::AppendErrorPages(http::HttpStatus status,
+                                    std::string filepath) {
+  if (error_pages_.find(status) == error_pages_.end()) {
+    error_pages_[status] = filepath;
+  }
+}
+
+bool LocationConf::GetAutoIndex() {
+  return auto_index_;
+}
+
+void LocationConf::SetAutoIndex(bool auto_index_is_enabled) {
+  auto_index_ = auto_index_is_enabled;
+}
+
+std::string LocationConf::GetRedirectUrl() {
+  return redirect_url_;
+}
+
+void LocationConf::SetRedirectUrl(std::string redirect_url) {
+  redirect_url_ = redirect_url;
+}
+
+bool LocationConf::IsMatchPattern(std::string path) {
+  if (is_backward_search_) {
+    return utils::BackwardMatch(path, path_pattern_);
+  } else {
+    return utils::ForwardMatch(path, path_pattern_);
+  }
+}
+
+};  // namespace config
--- srcs/config/location_conf.hpp
+++ srcs/config/location_conf.hpp
@@ -1,6 +1,8 @@
 #ifndef CONFIG_LOCATION_CONF
 #define CONFIG_LOCATION_CONF
 
+#include <stdint.h>
+
 #include <map>
 #include <set>
 #include <string>
@@ -13,10 +15,12 @@ namespace config {
 // serverディレクティブ内のlocationディレクティブの情報
 class LocationConf {
  private:
-  std::set<std::string> allow_methods_;
-  int64_t client_max_body_size_;
-  std::string root_;
-  std::vector<std::string> index_;
+  std::string path_pattern_;
+  bool is_backward_search_;
+  std::set<std::string> allowed_methods_;
+  int64_t client_max_body_size_kb_;
+  std::string root_dir_;
+  std::vector<std::string> index_pages_;
   bool is_cgi_;
   // errorPages[<status_code>] = <error_page_path>
   std::map<http::HttpStatus, std::string> error_pages_;
@@ -24,6 +28,57 @@ class LocationConf {
   bool auto_index_;
   // returnディレクティブで指定されたURL
   std::string redirect_url_;
+
+ public:
+  LocationConf();
+
+  LocationConf(const LocationConf &rhs);
+
+  LocationConf &operator=(const LocationConf &rhs);
+
+  ~LocationConf();
+
+  std::string GetPathPattern();
+
+  void SetPathPattern(std::string path_pattern);
+
+  bool GetIsBackwardSearch();
+
+  void SetIsBackwardSearch(bool is_backward_search);
+
+  bool IsMethodAllowed(std::string method);
+
+  void AppendAllowedMethod(std::string method);
+
+  int64_t GetClientMaxBodySizeKB();
+
+  void SetClientMaxBodySizeKB(int64_t client_max_body_size_kb);
+
+  std::string GetRootDir();
+
+  void SetRootDir(std::string root_dir);
+
+  const std::vector<std::string> &GetIndexPages();
+
+  void AppendIndexPages(std::string filepath);
+
+  bool GetIsCgi();
+
+  void SetIsCgi(bool is_cgi);
+
+  const std::map<http::HttpStatus, std::string> &GetErrorPages();
+
+  void AppendErrorPages(http::HttpStatus status, std::string filepath);
+
+  bool GetAutoIndex();
+
+  void SetAutoIndex(bool auto_index_is_enabled);
+
+  std::string GetRedirectUrl();
+
+  void SetRedirectUrl(std::string redirect_url);
+
+  bool IsMatchPattern(std::string path);
 };
 
 };  // namespace config
