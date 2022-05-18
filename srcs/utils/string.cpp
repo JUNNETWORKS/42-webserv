3a59bbd2a3cad086505a2c026c895d835a912b97
Junichi Sasaki <junichi200123@gmail.com>
2022-05-18 17:52:53 +0900

    utils::ForwardMatch() と utils::BackwardMatch() を実装

---
+++ srcs/utils/string.cpp
@@ -0,0 +1,12 @@
+#include "utils/string.hpp"
+
+namespace utils {
+
+bool ForwardMatch(std::string str, std::string pattern) {
+  return str.find(pattern) == 0;
+}
+bool BackwardMatch(std::string str, std::string pattern) {
+  return str.rfind(pattern) == str.length() - pattern.length();
+}
+
+}  // namespace utils
