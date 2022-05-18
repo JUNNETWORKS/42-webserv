7c7cf9e3161b4ff137b0c601f12aea2660016ea3
Junichi Sasaki <junichi200123@gmail.com>
2022-05-19 00:38:42 +0900

    新規接続処理をLogConnectionInfoToStdout()にまとめた

--- srcs/server/event_loop.cpp
+++ srcs/server/event_loop.cpp
@@ -1,7 +1,7 @@
-
 #include "server/event_loop.hpp"
 
 #include "config/config.hpp"
+#include "utils/error.hpp"
 #include "utils/inet_sockets.hpp"
 
 namespace server {
@@ -19,6 +19,14 @@ int AddSocketFdIntoEpfd(int epfd, int sockfd, SocketInfo::ESockType socktype,
       SocketInfo::Request;  // 新規接続は皆Requestから始まる
   epev->events = epevents;
   return epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, epev);
+}
+
+void LogConnectionInfoToStdout(struct sockaddr_storage &client_addr) {
+  socklen_t len = sizeof(struct sockaddr_storage);
+  char addrStr[utils::IS_ADDR_STR_LEN];
+  utils::inetAddressStr((struct sockaddr *)&client_addr, len, addrStr,
+                        utils::IS_ADDR_STR_LEN);
+  printf("Connection from %s\n", addrStr);
 }
 
 }  // namespace
@@ -49,17 +57,11 @@ int StartEventLoop(int listen_fd) {
       int conn_fd =
           accept(listen_fd, (struct sockaddr *)&client_addr, &addrlen);
       fcntl(conn_fd, F_SETFD, O_NONBLOCK);
-      if (AddSocketFdIntoEpfd(epfd, conn_fd, SocketInfo::ConnSock,
-                              EPOLLIN | EPOLLOUT) == -1) {
-        exit(EXIT_FAILURE);
+      if (AddSocketFdIntoEpfd(epfd, conn_fd, SocketInfo::ConnSock, EPOLLIN) ==
+          -1) {
+        utils::ErrExit("AddSocketFdIntoEpFd");
       }
-
-      socklen_t len = sizeof(struct sockaddr_storage);
-      char addrStr[utils::IS_ADDR_STR_LEN];
-      utils::inetAddressStr((struct sockaddr *)&client_addr, len, addrStr,
-                            utils::IS_ADDR_STR_LEN);
-      printf("Connection from %s\n", addrStr);
-      sleep(2);
+      LogConnectionInfoToStdout(client_addr);
     } else {
       int conn_fd = socket_info->fd;
 
@@ -79,17 +81,12 @@ int StartEventLoop(int listen_fd) {
           printf("Received data: %s\n", buf);
         }
       }
+
       // if space in write buffer, read
       if (epevarr[0].events & EPOLLOUT) {
-        // const char *str = "Some buffer or sendfile().\n";
-        // int n = write(conn_fd, str, strlen(str) - 1);
-        // if (n < static_cast<int>(strlen(str))) {
-        //   // TODO: ソケットバッファの都合などですべて書き込めない場合がある
-        //   printf("can't write all contents in buffer: %d/%lu\n", n,
-        //          strlen(str));
-        // } else {
-        // }
+        // TODO: Send HTTP Response to the client
       }
+
       // error or timeout? close conn_fd and remove from epfd
       if (epevarr[0].events & (EPOLLERR | EPOLLHUP)) {
         printf("Connection error\n");
