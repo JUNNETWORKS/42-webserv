021fb8ec66eac74b0ec0873f97090c7faedb81eb
Taisei Toya <tytaisei0102@gmail.com>
2022-05-23 20:26:58 +0200

    http_constantsを追加

---
+++ srcs/http/http_constants.hpp
@@ -0,0 +1,12 @@
+#ifndef HTTP_CONSTANTS_HPP_
+#define HTTP_CONSTANTS_HPP_
+
+#include <string>
+
+namespace http {
+const std::string kCrlf = "\r\n";
+const std::string kHeaderBoundary = kCrlf + kCrlf;
+}  // namespace http
+
+#endif /* ************************************************** HTTP_CONSTANTS_H \
+        */
