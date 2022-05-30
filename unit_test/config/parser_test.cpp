7baaff11193e7333322407dcfe61c7bd662284f4
Junichi Sasaki <junichi200123@gmail.com>
2022-05-30 17:43:21 +0900

    ParserTestで試す用の入力をファイルへのパスを入力とする

--- unit_test/config/parser_test.cpp
+++ unit_test/config/parser_test.cpp
@@ -6,21 +6,56 @@ namespace config {
 
 namespace {
 const int64_t kDefaultClientMaxBodySize = 1024 * 1024;  // 1MB
+const std::string kConfigurationDirPath = "unit_test/config/configurations/";
 }  // namespace
+
+TEST(ParserTest, FileNameIsInvalid) {
+  Parser parser;
+  EXPECT_THROW(parser.LoadFile(kConfigurationDirPath + "NotExists.conf");
+               , Parser::ParserException);
+}
 
 TEST(ParserTest, SimpleServer) {
   Parser parser;
-  parser.LoadData(
-      "server {"
-      "listen 8080;"
+  parser.LoadFile(kConfigurationDirPath + "SimpleServer.conf");
+  Config config = parser.ParseConfig();
+  config.Print();
+  EXPECT_TRUE(config.IsValid());
 
-      "location / {"
-      "allow_method GET;"
-      "root /var/www/html;"
-      "index index.html;"
-      "error_page 404 403 NotFound.html;"
-      "}"
-      "}");
+  const VirtualServerConf *vserver = config.GetVirtualServerConf("8080", "");
+  ASSERT_TRUE(vserver != NULL);
+  EXPECT_TRUE(vserver->GetListenPort() == "8080");
+
+  const LocationConf *location = vserver->GetLocation("/");
+  ASSERT_TRUE(location != NULL);
+  EXPECT_TRUE(location->GetPathPattern() == "/");
+  EXPECT_TRUE(location->GetIsBackwardSearch() == false);
+  EXPECT_TRUE(location->IsMethodAllowed("GET") == true);
+  EXPECT_TRUE(location->IsMethodAllowed("POST") == false);
+  EXPECT_TRUE(location->IsMethodAllowed("DELETE") == false);
+  EXPECT_TRUE(location->GetClientMaxBodySize() == kDefaultClientMaxBodySize);
+  EXPECT_TRUE(location->GetRootDir() == "/var/www/html");
+  EXPECT_TRUE(location->GetIndexPages() ==
+              LocationConf::IndexPagesVector{"index.html"});
+  EXPECT_TRUE(location->GetIsCgi() == false);
+  LocationConf::ErrorPagesMap errorpages = LocationConf::ErrorPagesMap{
+      std::make_pair<LocationConf::ErrorPagesMap::key_type,
+                     LocationConf::ErrorPagesMap::mapped_type>(
+          static_cast<LocationConf::ErrorPagesMap::key_type>(404),
+          "NotFound.html"),
+      std::make_pair<LocationConf::ErrorPagesMap::key_type,
+                     LocationConf::ErrorPagesMap::mapped_type>(
+          static_cast<LocationConf::ErrorPagesMap::key_type>(403),
+          "NotFound.html"),
+  };
+  EXPECT_TRUE(location->GetErrorPages() == errorpages);
+  EXPECT_TRUE(location->GetAutoIndex() == false);
+  EXPECT_TRUE(location->GetRedirectUrl() == "");
+}
+
+TEST(ParserTest, SimpleServerInOneLine) {
+  Parser parser;
+  parser.LoadFile(kConfigurationDirPath + "SimpleServerInOneLine.conf");
   Config config = parser.ParseConfig();
   config.Print();
   EXPECT_TRUE(config.IsValid());
@@ -58,17 +93,7 @@ TEST(ParserTest, SimpleServer) {
 
 TEST(ParserTest, EscapedChar) {
   Parser parser;
-  parser.LoadData(
-      "server {"
-      "listen 8080;"
-
-      "location / {"
-      "allow_method GET;"
-      "root /var/www/h\\tml;"
-      "index index.\\;ht\\\\ml;"
-      "error_page 404 403 Not\\ Found.html;"
-      "}"
-      "}");
+  parser.LoadFile(kConfigurationDirPath + "EscapedChar.conf");
   Config config = parser.ParseConfig();
   config.Print();
 
@@ -122,6 +147,6 @@ TEST(ParserTest, HttpStatusInErrorPagesAreInvalid) {}
 
 TEST(ParserTest, HttpStatusInErrorPagesAreInvalid){}
 
-    * /
+*/
 
 };  // namespace config
