a232d50e895621af3ff49430480c0ff0c8734ac6
Junichi Sasaki <junichi200123@gmail.com>
2022-05-30 19:04:47 +0900

    ParserTest::MultipleValidServers を追加

--- unit_test/config/parser_test.cpp
+++ unit_test/config/parser_test.cpp
@@ -121,9 +121,159 @@ TEST(ParserTest, EscapedChar) {
   EXPECT_TRUE(config.IsValid());
 }
 
-/*
+TEST(ParserTest, MultipleValidServers) {
+  Parser parser;
+  parser.LoadFile(kConfigurationDirPath + "MultipleValidServers.conf");
+  Config config = parser.ParseConfig();
+  config.Print();
 
-TEST(ParserTest, TwoServerHaveSamePortWithDifferentServername) {}
+  {
+    const VirtualServerConf *vserver =
+        config.GetVirtualServerConf("80", "localhost");
+    ASSERT_TRUE(vserver != NULL);
+
+    {
+      const LocationConf *location = vserver->GetLocation("/");
+      ASSERT_TRUE(location != NULL);
+      EXPECT_TRUE(location->GetPathPattern() == "/");
+      EXPECT_TRUE(location->GetIsBackwardSearch() == false);
+      EXPECT_TRUE(location->IsMethodAllowed("GET") == true);
+      EXPECT_TRUE(location->IsMethodAllowed("POST") == false);
+      EXPECT_TRUE(location->IsMethodAllowed("DELETE") == false);
+      EXPECT_TRUE(location->GetRootDir() == "/var/www/html");
+      LocationConf::IndexPagesVector indexpages =
+          LocationConf::IndexPagesVector{"index.html", "index.htm"};
+      EXPECT_TRUE(location->GetIndexPages() == indexpages);
+      LocationConf::ErrorPagesMap errorpages = LocationConf::ErrorPagesMap{
+          std::make_pair<LocationConf::ErrorPagesMap::key_type,
+                         LocationConf::ErrorPagesMap::mapped_type>(
+              static_cast<LocationConf::ErrorPagesMap::key_type>(500),
+              "server_error_page.html"),
+          std::make_pair<LocationConf::ErrorPagesMap::key_type,
+                         LocationConf::ErrorPagesMap::mapped_type>(
+              static_cast<LocationConf::ErrorPagesMap::key_type>(404),
+              "not_found.html"),
+          std::make_pair<LocationConf::ErrorPagesMap::key_type,
+                         LocationConf::ErrorPagesMap::mapped_type>(
+              static_cast<LocationConf::ErrorPagesMap::key_type>(403),
+              "not_found.html"),
+      };
+      EXPECT_TRUE(location->GetErrorPages() == errorpages);
+      EXPECT_TRUE(location->GetRedirectUrl() == "");
+      EXPECT_TRUE(location->GetAutoIndex() == false);
+      EXPECT_TRUE(location->GetIsCgi() == false);
+      EXPECT_TRUE(config.IsValid());
+    }
+
+    {
+      const LocationConf *location = vserver->GetLocation("/upload");
+      ASSERT_TRUE(location != NULL);
+      EXPECT_TRUE(location->GetPathPattern() == "/upload");
+      EXPECT_TRUE(location->GetIsBackwardSearch() == false);
+      EXPECT_TRUE(location->IsMethodAllowed("GET") == true);
+      EXPECT_TRUE(location->IsMethodAllowed("POST") == true);
+      EXPECT_TRUE(location->IsMethodAllowed("DELETE") == true);
+      EXPECT_TRUE(location->GetRootDir() == "/var/www/user_uploads");
+      LocationConf::IndexPagesVector indexpages =
+          LocationConf::IndexPagesVector{};
+      EXPECT_TRUE(location->GetIndexPages() == indexpages);
+      LocationConf::ErrorPagesMap errorpages = LocationConf::ErrorPagesMap{};
+      EXPECT_TRUE(location->GetErrorPages() == errorpages);
+      EXPECT_TRUE(location->GetClientMaxBodySize() == 1073741824);
+      EXPECT_TRUE(location->GetRedirectUrl() == "");
+      EXPECT_TRUE(location->GetAutoIndex() == true);
+      EXPECT_TRUE(location->GetIsCgi() == false);
+      EXPECT_TRUE(config.IsValid());
+    }
+  }
+
+  {
+    const VirtualServerConf *vserver =
+        config.GetVirtualServerConf("80", "www.webserv.com");
+    ASSERT_TRUE(vserver != NULL);
+
+    {
+      const LocationConf *location = vserver->GetLocation("/");
+      ASSERT_TRUE(location != NULL);
+      EXPECT_TRUE(location->GetPathPattern() == "/");
+      EXPECT_TRUE(location->GetIsBackwardSearch() == false);
+      EXPECT_TRUE(location->IsMethodAllowed("GET") == false);
+      EXPECT_TRUE(location->IsMethodAllowed("POST") == false);
+      EXPECT_TRUE(location->IsMethodAllowed("DELETE") == false);
+      EXPECT_TRUE(location->GetRootDir() == "/var/www/html");
+      LocationConf::IndexPagesVector indexpages =
+          LocationConf::IndexPagesVector{"index.html"};
+      EXPECT_TRUE(location->GetIndexPages() == indexpages);
+      LocationConf::ErrorPagesMap errorpages = LocationConf::ErrorPagesMap{};
+      EXPECT_TRUE(location->GetErrorPages() == errorpages);
+      EXPECT_TRUE(location->GetClientMaxBodySize() ==
+                  kDefaultClientMaxBodySize);
+      EXPECT_TRUE(location->GetRedirectUrl() == "");
+      EXPECT_TRUE(location->GetAutoIndex() == false);
+      EXPECT_TRUE(location->GetIsCgi() == false);
+      EXPECT_TRUE(config.IsValid());
+    }
+
+    {
+      const LocationConf *location = vserver->GetLocation(".php");
+      ASSERT_TRUE(location != NULL);
+      EXPECT_TRUE(location->GetPathPattern() == ".php");
+      EXPECT_TRUE(location->GetIsBackwardSearch() == true);
+      EXPECT_TRUE(location->IsMethodAllowed("GET") == false);
+      EXPECT_TRUE(location->IsMethodAllowed("POST") == false);
+      EXPECT_TRUE(location->IsMethodAllowed("DELETE") == false);
+      EXPECT_TRUE(location->GetRootDir() == "/home/nginx/cgi_bins");
+      LocationConf::IndexPagesVector indexpages =
+          LocationConf::IndexPagesVector{};
+      EXPECT_TRUE(location->GetIndexPages() == indexpages);
+      LocationConf::ErrorPagesMap errorpages = LocationConf::ErrorPagesMap{};
+      EXPECT_TRUE(location->GetErrorPages() == errorpages);
+      EXPECT_TRUE(location->GetClientMaxBodySize() ==
+                  kDefaultClientMaxBodySize);
+      EXPECT_TRUE(location->GetRedirectUrl() == "");
+      EXPECT_TRUE(location->GetAutoIndex() == false);
+      EXPECT_TRUE(location->GetIsCgi() == true);
+      EXPECT_TRUE(config.IsValid());
+    }
+
+    EXPECT_TRUE(config.IsValid());
+  }
+
+  {
+    const VirtualServerConf *vserver = config.GetVirtualServerConf("8080", "");
+    ASSERT_TRUE(vserver != NULL);
+
+    const LocationConf *location = vserver->GetLocation("/");
+    ASSERT_TRUE(location != NULL);
+    EXPECT_TRUE(location->GetPathPattern() == "/");
+    EXPECT_TRUE(location->GetRootDir() == "/var/www/html");
+    LocationConf::IndexPagesVector indexpages =
+        LocationConf::IndexPagesVector{"index.html"};
+    EXPECT_TRUE(location->GetIndexPages() == indexpages);
+    LocationConf::ErrorPagesMap errorpages = LocationConf::ErrorPagesMap{};
+    EXPECT_TRUE(location->GetErrorPages() == errorpages);
+    EXPECT_TRUE(location->GetRedirectUrl() == "");
+    EXPECT_TRUE(config.IsValid());
+  }
+
+  {
+    const VirtualServerConf *vserver = config.GetVirtualServerConf("9090", "");
+    ASSERT_TRUE(vserver != NULL);
+    const LocationConf *location = vserver->GetLocation("/");
+    ASSERT_TRUE(location != NULL);
+    EXPECT_TRUE(location->GetPathPattern() == "/");
+    EXPECT_TRUE(location->GetRootDir() == "");
+    LocationConf::IndexPagesVector indexpages =
+        LocationConf::IndexPagesVector{};
+    EXPECT_TRUE(location->GetIndexPages() == indexpages);
+    LocationConf::ErrorPagesMap errorpages = LocationConf::ErrorPagesMap{};
+    EXPECT_TRUE(location->GetErrorPages() == errorpages);
+    EXPECT_TRUE(location->GetRedirectUrl() == "http://localhost:8080/");
+    EXPECT_TRUE(config.IsValid());
+  }
+}
+
+/*
 
 TEST(ParserTest, LocationDoesntHaveRoot) {}
 
