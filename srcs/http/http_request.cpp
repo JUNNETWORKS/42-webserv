d5ae9d0830e6407ff484a9c0e863b13649e171a3
Taisei Toya <tytaisei0102@gmail.com>
2022-06-01 12:00:16 +0200

    ParseBodySizeに入ることがなかったので修正

--- srcs/http/http_request.cpp
+++ srcs/http/http_request.cpp
@@ -79,7 +79,7 @@ HttpRequest::ParsingPhase HttpRequest::ParseHeaderField() {
     if (buffer_.CompareHead(kHeaderBoundary)) {
       buffer_.EraseHead(kHeaderBoundary.size());
       //先頭が\r\n\r\nなので終了処理
-      return kBody;
+      return kBodySize;
     }
 
     if (buffer_.CompareHead(kCrlf)) {
