8ee779c350482915c5e2a50faf3a1ecb1f65cb99
Junichi Sasaki <junichi200123@gmail.com>
2022-06-04 05:33:56 +0900

    event_loop.cpp 内で SocketManager を利用する｡

--- srcs/server/event_loop.cpp
+++ srcs/server/event_loop.cpp
@@ -1,7 +1,7 @@
 #include "server/event_loop.hpp"
 
 #include "config/config.hpp"
-#include "server/event_manager.hpp"
+#include "server/socket_manager.hpp"
 #include "utils/error.hpp"
 #include "utils/inet_sockets.hpp"
 
@@ -10,14 +10,14 @@ namespace {
 
 const int BUF_SIZE = 1024;
 
-void ProcessRequest(int conn_fd, int epfd, SocketInfo *info) {
+void ProcessRequest(SocketManager &socket_manager, SocketInfo *info) {
   unsigned char buf[BUF_SIZE];
+  int conn_fd = info->fd;
   int n = read(conn_fd, buf, sizeof(buf) - 1);
   if (n <= 0) {  // EOF(Connection end) or Error
                  // TODO ここいらないかも？
     printf("Connection end\n");
-    close(conn_fd);
-    epoll_ctl(epfd, EPOLL_CTL_DEL, conn_fd, NULL);  // 明示的に消してる
+    socket_manager.CloseConnFd(conn_fd);
   } else {
     info->buffer_.AppendDataToBuffer(buf, n);
 
@@ -36,6 +36,20 @@ void ProcessRequest(int conn_fd, int epfd, SocketInfo *info) {
   }
 }
 
+void ProcessResponse(SocketManager &socket_manager, SocketInfo *info) {
+  // TODO: Send HTTP Response to the client
+  int conn_fd = info->fd;
+  if (info->requests.front().IsCorrectRequest()) {
+    info->response.SetStatusLine("HTTP/1.1 200 OK");
+    // socket_info->response.setHeader("Content-Type: text/plain");
+    info->response.LoadFile(info->requests.front().GetPath());
+    info->response.Write(conn_fd);
+    socket_manager.CloseConnFd(conn_fd);
+  } else {
+    socket_manager.CloseConnFd(conn_fd);
+  }
+}
+
 }  // namespace
 
 int StartEventLoop(const std::vector<int> &listen_fds,
@@ -44,45 +58,34 @@ int StartEventLoop(const std::vector<int> &listen_fds,
   (void)config;
 
   // epoll インスタンス作成
-  EventManager event_manager;
+  SocketManager socket_manager;
 
-  event_manager.AppendListenFd(listen_fds);
+  socket_manager.AppendListenFd(listen_fds);
 
   // イベントループ
   while (1) {
-    struct epoll_event epev = event_manager.WaitEvent();
+    struct epoll_event epev = socket_manager.WaitEvent();
     SocketInfo *socket_info = static_cast<SocketInfo *>(epev.data.ptr);
 
     if (socket_info->socktype == SocketInfo::ListenSock) {
-      if (!event_manager.AcceptNewConnection(socket_info->fd)) {
+      if (!socket_manager.AcceptNewConnection(socket_info->fd)) {
         utils::ErrExit("AddSocketFdIntoEpFd");
       }
     } else {
       int conn_fd = socket_info->fd;
       // if data in read buffer, read
       if (epev.events & EPOLLIN) {
-        ProcessRequest(conn_fd, event_manager.GetEpollFd(), socket_info);
+        ProcessRequest(socket_manager, socket_info);
       }
       // if space in write buffer, read
       if (epev.events & EPOLLOUT) {
-        // TODO: Send HTTP Response to the client
-        if (socket_info->requests.front().IsCorrectRequest()) {
-          socket_info->response.SetStatusLine("HTTP/1.1 200 OK");
-          // socket_info->response.setHeader("Content-Type: text/plain");
-          socket_info->response.LoadFile(
-              socket_info->requests.front().GetPath());
-          socket_info->response.Write(conn_fd);
-          close(conn_fd);
-        } else {
-          close(conn_fd);
-        }
+        ProcessResponse(socket_manager, socket_info);
       }
 
       // error or timeout? close conn_fd and remove from epfd
       if (epev.events & (EPOLLERR | EPOLLHUP)) {
         printf("Connection error\n");
-        close(conn_fd);
-        event_manager.RemoveFd(conn_fd);  // 明示的に消してる
+        socket_manager.CloseConnFd(conn_fd);
       }
     }
   }
