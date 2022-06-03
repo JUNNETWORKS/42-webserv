d400321d39107b09c0c9cbfab66bf34c6626f930
Junichi Sasaki <junichi200123@gmail.com>
2022-06-03 19:27:06 +0900

    Stoulのテストで引数の順番を修正

--- unit_test/utils/string_test.cpp
+++ unit_test/utils/string_test.cpp
@@ -6,66 +6,66 @@ namespace utils {
 
 TEST(StoulTest, Zero) {
   unsigned long num;
-  EXPECT_TRUE(Stoul("0", num));
+  EXPECT_TRUE(Stoul(num, "0"));
   EXPECT_EQ(num, 0);
 }
 
 TEST(StoulTest, Maximumvalue) {
   unsigned long num;
-  EXPECT_TRUE(Stoul("18446744073709551615", num));
+  EXPECT_TRUE(Stoul(num, "18446744073709551615"));
   EXPECT_EQ(num, 18446744073709551615ul);
 }
 
 TEST(StoulTest, MaximumvalueWithPlusSign) {
   unsigned long num;
-  EXPECT_TRUE(Stoul("+18446744073709551615", num));
+  EXPECT_TRUE(Stoul(num, "+18446744073709551615"));
   EXPECT_EQ(num, 18446744073709551615ul);
 }
 
 TEST(StoulTest, MaximumvaluePlusOne) {
   unsigned long num;
-  EXPECT_FALSE(Stoul("18446744073709551616", num));
+  EXPECT_FALSE(Stoul(num, "18446744073709551616"));
 }
 
 TEST(StoulTest, MaximumvalueWithPlusSigns) {
   unsigned long num;
-  EXPECT_FALSE(Stoul("+++++++++++18446744073709551615", num));
+  EXPECT_FALSE(Stoul(num, "+++++++++++18446744073709551615"));
 }
 
 TEST(StoulTest, MinusOne) {
   unsigned long num;
-  EXPECT_FALSE(Stoul("-1", num));
+  EXPECT_FALSE(Stoul(num, "-1"));
 }
 
 TEST(StoulTest, MinusSigns) {
   unsigned long num;
-  EXPECT_FALSE(Stoul("--------1", num));
+  EXPECT_FALSE(Stoul(num, "--------1"));
 }
 
 TEST(StoulTest, PlusZero) {
   unsigned long num;
-  EXPECT_TRUE(Stoul("+0", num));
+  EXPECT_TRUE(Stoul(num, "+0"));
   EXPECT_EQ(num, 0);
 }
 
 TEST(StoulTest, MinusZero) {
   unsigned long num;
-  EXPECT_FALSE(Stoul("-0", num));
+  EXPECT_FALSE(Stoul(num, "-0"));
 }
 
 TEST(StoulTest, IncludeAlphabetFront) {
   unsigned long num;
-  EXPECT_FALSE(Stoul("a100", num));
+  EXPECT_FALSE(Stoul(num, "a100"));
 }
 
 TEST(StoulTest, IncludeAlphabetBack) {
   unsigned long num;
-  EXPECT_FALSE(Stoul("100a", num));
+  EXPECT_FALSE(Stoul(num, "100a"));
 }
 
 TEST(StoulTest, IncludeAlphabetMiddle) {
   unsigned long num;
-  EXPECT_FALSE(Stoul("10a0", num));
+  EXPECT_FALSE(Stoul(num, "10a0"));
 }
 
 }  // namespace utils
