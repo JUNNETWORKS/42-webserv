45a8876c6852ef91fab61258f59fc0a5ac7bafda
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2022-06-05 05:16:47 +0900

    haedとbodyに分けてテストするようにした。

--- test/client.py
+++ test/client.py
@@ -35,31 +35,85 @@ def get_file_data(file_path):
     return file_data
 
 
-def run_test(send_data):
+def separate_head_body(s):
+    pos = s.find("\r\n\r\n")
+    if pos == -1:
+        return s, ""
+    else:
+        return s[:pos], s[pos + 4 :]
+
+
+def check_body(webserv_res_body, nginx_res_body):
+    if webserv_res_body == nginx_res_body:
+        return True
+    else:
+        return False
+
+
+# ステータスラインのみチェック
+def check_status_line(webserv_res_head, nginx_res_head):
+    webserv_res_status_line = ""
+    nginx_res_status_line = ""
+
+    for line in webserv_res_head.split("\n"):
+        if "HTTP" in line:
+            webserv_res_status_line += line + "\n"
+
+    for line in nginx_res_head.split("\n"):
+        if "HTTP" in line:
+            nginx_res_status_line += line + "\n"
+
+    if webserv_res_status_line == nginx_res_status_line:
+        return True
+    else:
+        return False
+
+
+# 複数のレスポンスの時に対応する必要あり
+def check_result(webserv_res, nginx_res):
+    webserv_res_head, webserv_res_body = separate_head_body(webserv_res)
+    nginx_res_head, nginx_res_body = separate_head_body(nginx_res)
+
+    is_status_line_ok = check_status_line(webserv_res_head, nginx_res_head)
+    is_message_body_ok = check_body(webserv_res_body, nginx_res_body)
+
+    print("STATUS LINE  :", is_status_line_ok)
+    print("MESSAGE BODY :", is_message_body_ok)
+
+    return is_status_line_ok and is_message_body_ok
+
+
+def lst_replace(s, replace_lst):
+    for set in replace_lst:
+        s = s.replace(set[0], set[1])
+    return s
+
+
+def run_test(test_file_path, replace_lst=[]):
+    send_data = get_file_data(test_file_path)
+    send_data = lst_replace(send_data, replace_lst)
+
+    print("TEST FILE    :", test_file_path, replace_lst)
     webserv_res = send_request(send_data, WEBSERV_PORT)
     nginx_res = send_request(send_data, NGINX_PORT)
 
-    print("--- WEBSERV RES ---")
-    # print(webserv_res)
-    print_response_status_code(webserv_res)
+    check_result(webserv_res, nginx_res)
+    print()
 
-    print("--- NGINX ---")
-    # print(nginx_res)
-    print_response_status_code(nginx_res)
+
+def path_test():
+    test_file_path = "req/tpl-get-path-test.txt"
+    run_test(test_file_path, [["{PATH}", "/hoge/hoge.html"]])
+    run_test(test_file_path, [["{PATH}", "/hoge/fuga.html"]])
 
 
 def main():
     if len(sys.argv) == 2:
         test_file = sys.argv[1]
+        run_test(test_file)
     else:
         test_file = "req/req1.txt"
-
-    print("TEST FILE :", test_file, file=sys.stderr)
-    print()
-
-    file_data = get_file_data(test_file)
-
-    run_test(file_data)
+        path_test()
 
 
 if __name__ == "__main__":
