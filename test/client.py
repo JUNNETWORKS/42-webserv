bcd1778bbd26650020e68ce77bb36c78e31aff58
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2022-06-05 00:35:55 +0900

    send_request の 引数をファイルパスから、送信するデータに変更

--- test/client.py
+++ test/client.py
@@ -7,13 +7,10 @@ WEBSERV_PORT = 49200
 NGINX_PORT = 49201
 
 
-def send_request(test_file, port):
+def send_request(send_data, port):
     s = socket.socket(socket.AF_INET)
     s.connect(("localhost", port))
-    with io.open(test_file, "r", newline="") as f:
-        a = f.read()
-        # print(a.encode("utf-8"))
-        s.send(a.encode("utf-8"))
+    s.send(send_data.encode("utf-8"))
     time.sleep(0.1)
 
     return s.recv(10000).decode("utf-8")
@@ -27,6 +24,13 @@ def print_response_status_code(res):
     print()
 
 
+def get_file_data(file_path):
+    file_data = ""
+    with io.open(file_path, "r", newline="") as f:
+        file_data = f.read()
+    return file_data
+
+
 def main():
     if len(sys.argv) == 2:
         test_file = sys.argv[1]
@@ -36,8 +40,10 @@ def main():
     print("TEST FILE :", test_file, file=sys.stderr)
     print()
 
-    webserv_res = send_request(test_file, WEBSERV_PORT)
-    nginx_res = send_request(test_file, NGINX_PORT)
+    file_data = get_file_data(test_file)
+
+    webserv_res = send_request(file_data, WEBSERV_PORT)
+    nginx_res = send_request(file_data, NGINX_PORT)
 
     print("--- WEBSERV RES ---")
     # print(webserv_res)
