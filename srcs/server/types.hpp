2662189dcc82bed5f660e52896f932296532a99a
Junichi Sasaki <junichi200123@gmail.com>
2022-06-05 15:07:06 +0900

    server/types.hpp にて ListenFdPortMap を定義
    listen_fd がListenしているポート番号を記録するためのMap

---
+++ srcs/server/types.hpp
@@ -0,0 +1,13 @@
+#ifndef SERVER_TYPES_HPP_
+#define SERVER_TYPES_HPP_
+
+#include <map>
+#include <string>
+
+namespace server {
+
+typedef std::map<int, std::string> ListenFdPortMap;
+
+}
+
+#endif
