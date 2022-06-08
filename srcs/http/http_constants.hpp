f4f561a7dc04cca76fcee0c3e2a546dc6899cb45
Taisei Toya <tytaisei0102@gmail.com>
2022-06-08 21:59:45 +0200

    ヘッダ名に不正な文字が入っていた時はじけるよう修正

--- srcs/http/http_constants.hpp
+++ srcs/http/http_constants.hpp
@@ -10,6 +10,7 @@ const std::string kHttpVersionPrefix = "HTTP/";
 const std::string kExpectMajorVersion = "1.";
 const int kMinorVersionDigitLimit = 3;
 const std::string kOWS = "\t ";
+const std::string kNotAlnumTchars = "!#$%&'*+-.^_`|~";
 }  // namespace http
 
 #endif
