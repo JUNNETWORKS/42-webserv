c918faf8e527839ebcd8428a1c35e3d8123cdb53
Junichi Sasaki <junichi200123@gmail.com>
2022-06-09 17:20:27 +0900

    FdEventにコンストラクタを追加

--- srcs/server/fd_event.hpp
+++ srcs/server/fd_event.hpp
@@ -1,6 +1,8 @@
 #ifndef SERVER_FILEDESCRIPTOR_HPP_
 #define SERVER_FILEDESCRIPTOR_HPP_
 
+namespace server {
+
 // Epoll クラスで利用する｡
 // fd の種類と発生したイベントの種類を保持する構造体
 struct FdEvent {
@@ -12,7 +14,13 @@ struct FdEvent {
   // epoll events
   unsigned int events;
 
+  FdEvent();
   FdEvent(int fd, EFdType fd_type, void *ptr);
+  FdEvent(const FdEvent &rhs);
+  const FdEvent &operator=(const FdEvent &rhs);
+  ~FdEvent();
 };
+
+}  // namespace server
 
 #endif
