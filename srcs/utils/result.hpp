2b8bc9fb9ce5e486721fdeacbc217d4c28366bbb
Junichi Sasaki <junichi200123@gmail.com>
2022-06-09 09:15:50 +0900

    ResultクラスとErrorクラスを実装

---
+++ srcs/utils/result.hpp
@@ -0,0 +1,112 @@
+#ifndef UTILS_RESULT_HPP_
+#define UTILS_RESULT_HPP_
+
+#include <string>
+
+namespace utils {
+
+// Result でエラーを表すためのクラス
+class Error {
+ private:
+  bool is_err_;
+  std::string err_msg_;
+
+ public:
+  Error(bool is_err = true);
+
+  // エラーメッセージを設定する｡
+  // is_err は true に設定される｡
+  Error(const std::string &err_msg);
+
+  Error(const Error &other);
+
+  const Error &operator=(const Error &rhs);
+
+  ~Error();
+
+  bool IsErr();
+  const std::string &Print();
+};
+
+// エラーが発生するかもしれない関数の返り値として利用する｡
+// T に参照は利用できない｡なぜならエラーの際に表す値が存在しないから｡
+//
+// ===== Usage =====
+// Result<int> succeed() {  // 成功
+//   return Result<int>(10);
+// }
+//
+// Result<void> succeedVoid() {  // 成功 (返り値がvoid)
+//   return Result<void>();
+// }
+//
+// Result<void> fail() {  // エラー
+//   return Result<void>(Error("This is error!"));
+// }
+//
+// int main(){
+//   Result<int> a = succeed();
+//   if (a.IsOk()) {
+//     std::cout << "val: " << a.Ok() << std::endl;
+//   }
+//   if (a.IsErr()) {
+//     std::cout << "err: " << a.Err().Print() << std::endl;
+//   }
+// }
+template <typename T>
+class Result {
+ private:
+  T val_;
+  Error err_;
+
+ public:
+  // デフォルトコンストラクタが存在しない場合にエラーを通知したい場合はこのコンストラクタを使う｡
+  Result(const T &val, const Error &err) : val_(val), err_(err) {}
+
+  Result(const T &val) : val_(val), err_(false) {}
+
+  // デフォルトコンストラクタが存在しない場合は使えない
+  Result(const Error &err) : val_(), err_(err) {}
+
+  bool IsOk() {
+    return !err_.IsErr();
+  }
+  T Ok() {
+    return val_;
+  }
+  bool IsErr() {
+    return err_.IsErr();
+  }
+  Error Err() {
+    return err_;
+  }
+};
+
+// T = void の場合はメンバー変数 val_ を保持することはできない｡
+//   Ok() も利用できない｡ エラーチェックのみ行うことができる｡
+//
+// 返り値が void 型の関数の返り値は利用しないことを考えれば適切な挙動である｡
+template <>
+class Result<void> {
+ private:
+  Error err_;
+
+ public:
+  Result() : err_(false) {}
+
+  Result(const Error &err) : err_(err) {}
+
+  bool IsOk() {
+    return !err_.IsErr();
+  }
+  bool IsErr() {
+    return err_.IsErr();
+  }
+  Error Err() {
+    return err_;
+  }
+};
+
+}  // namespace utils
+
+#endif
