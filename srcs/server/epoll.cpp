74b4055e61d11e8b4a421ae9bf50f08cef809ded
Junichi Sasaki <junichi200123@gmail.com>
2022-06-11 00:14:03 +0900

    Epoll では FdEvent を受け取る

--- srcs/server/epoll.cpp
+++ srcs/server/epoll.cpp
@@ -5,7 +5,7 @@
 #include <cassert>
 #include <vector>
 
-#include "server/file_descriptor.hpp"
+#include "server/fd_event.hpp"
 #include "utils/error.hpp"
 
 namespace server {
@@ -20,38 +20,48 @@ Epoll::~Epoll() {
   close(epfd_);
 }
 
-bool Epoll::AddFd(const FileDescriptor &file_descriptor, unsigned int events) {
+bool Epoll::AddFd(const FdEvent &fd_event, unsigned int events) {
   struct epoll_event epev;
   epev.events = events;
-  epev.data.fd = file_descriptor.fd;
-  bool is_success =
-      (epoll_ctl(epfd_, EPOLL_CTL_ADD, file_descriptor.fd, &epev) == 0);
+  epev.data.fd = fd_event.fd;
+
+  bool is_success = (epoll_ctl(epfd_, EPOLL_CTL_ADD, fd_event.fd, &epev) == 0);
   if (is_success) {
-    registered_fds_[file_descriptor.fd] = file_descriptor;
+    registered_fd_events_[fd_event.fd] = fd_event;
   }
   return is_success;
 }
 
 bool Epoll::RemoveFd(int fd) {
-  registered_fds_.erase(fd);
+  registered_fd_events_.erase(fd);
   return epoll_ctl(epfd_, EPOLL_CTL_DEL, fd, NULL) == 0;
 }
 
-bool Epoll::ModifyFd(const FileDescriptor &file_descriptor,
-                     unsigned int events) {
+bool Epoll::ModifyFd(const FdEvent &fd_event, unsigned int events) {
+  if (registered_fd_events_.find(fd_event.fd) == registered_fd_events_.end()) {
+    return false;
+  }
+  registered_fd_events_[fd_event.fd] = fd_event;
+
   struct epoll_event epev;
   epev.events = events;
-  epev.data.fd = file_descriptor.fd;
-  return epoll_ctl(epfd_, EPOLL_CTL_MOD, file_descriptor.fd, &epev) == 0;
+  epev.data.fd = fd_event.fd;
+  return epoll_ctl(epfd_, EPOLL_CTL_MOD, fd_event.fd, &epev) == 0;
 }
 
-bool Epoll::WaitEvents(std::vector<const FileDescriptor &> &events,
-                       int timeout_ms = -1) {
-  int maxevents = registered_fds_.size();
+bool Epoll::WaitEvents(std::vector<FdEvent> &events, int timeout_ms) {
+  int maxevents = registered_fd_events_.size();
   struct epoll_event *event_arr = new struct epoll_event[maxevents];
   int event_num = epoll_wait(epfd_, event_arr, maxevents, timeout_ms);
+
   if (event_num > 0) {
-    events.insert(events.end(), event_arr, event_arr + event_num);
+    for (int i = 0; i < event_num; ++i) {
+      assert(registered_fd_events_.find(event_arr[i].data.fd) !=
+             registered_fd_events_.end());
+      FdEvent fd_event = registered_fd_events_[event_arr[i].data.fd];
+      fd_event.events = event_arr[i].events;
+      events.push_back(fd_event);
+    }
   }
   delete[] event_arr;
   return event_num > 0;
--- srcs/server/epoll.hpp
+++ srcs/server/epoll.hpp
@@ -6,7 +6,7 @@
 #include <map>
 #include <vector>
 
-#include "server/file_descriptor.hpp"
+#include "server/fd_event.hpp"
 
 namespace server {
 
@@ -14,23 +14,22 @@ class Epoll {
  private:
   const int epfd_;
 
-  std::map<int, FileDescriptor> registered_fds_;
+  // map[<fd>] = <FdEvent>
+  std::map<int, FdEvent> registered_fd_events_;
 
  public:
   Epoll();
   ~Epoll();
 
   // 購読するファイルディスクリプタを追加
-  // file_descriptor は Epoll クラス内で保持される｡
-  bool AddFd(const FileDescriptor &file_descriptor, unsigned int events);
+  bool AddFd(const FdEvent &fd_event, unsigned int events);
 
   // 購読しているファイルディスクリプタを削除
   bool RemoveFd(int fd);
 
   // 購読しているファイルディスクリプタの購読情報を変更
-  // file_descriptor は Epoll クラス内で保持される｡
   // fd が Epoll に存在しない場合やエラーの場合は false を返す｡
-  bool ModifyFd(const FileDescriptor &file_descriptor, unsigned int events);
+  bool ModifyFd(const FdEvent &fd_event, unsigned int events);
 
   // 利用可能なイベントをepoll_waitで取得し､eventsの末尾に挿入する｡
   //
@@ -39,8 +38,7 @@ class Epoll {
   //
   // イベントが1つ以上 events に追加されたら true を返す｡
   // タイムアウトやエラーの場合は false を返す｡
-  bool WaitEvents(std::vector<const FileDescriptor &> &events,
-                  int timeout_ms = -1);
+  bool WaitEvents(std::vector<FdEvent> &events, int timeout_ms = -1);
 
  private:
   // epoll instance が片方のみでcloseされるのを防ぐためコピー操作は禁止
