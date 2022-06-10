0d9ebc50b20752d508460f85bdc8cd80eefefae4
Junichi Sasaki <junichi200123@gmail.com>
2022-06-11 00:18:51 +0900

    Socketクラスを作成
    ゆくゆくはSocketManagerを廃止してSocketクラスをFdEvent.ptrに入れるようにしようと考えている｡

---
+++ srcs/server/socket.cpp
@@ -0,0 +1,95 @@
+#include "server/socket.hpp"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <cassert>
+
+#include "utils/inet_sockets.hpp"
+
+namespace server {
+
+std::map<int, int> Socket::fd_reference_counter_;
+
+Socket::Socket(int fd, ESockType socktype, const std::string &port)
+    : fd_(fd), socktype_(socktype), port_(port) {
+  IncreaseFdReferenceCount(fd_);
+}
+
+Socket::Socket(const Socket &rhs) {
+  *this = rhs;
+}
+
+Socket &Socket::operator=(const Socket &rhs) {
+  if (this != &rhs) {
+    DecreaseFdReferenceCount(fd_);
+
+    fd_ = rhs.fd_;
+    socktype_ = rhs.socktype_;
+    port_ = rhs.port_;
+    requests_ = rhs.requests_;
+    requests_ = rhs.requests_;
+
+    IncreaseFdReferenceCount(fd_);
+  }
+  return *this;
+}
+
+Socket::~Socket() {
+  DecreaseFdReferenceCount(fd_);
+  if (GetFdReferenceCount(fd_) == 0) {
+    close(fd_);
+  }
+}
+
+Socket *Socket::AcceptNewConnection() {
+  assert(socktype_ == ListenSock);
+
+  struct sockaddr_storage client_addr;
+  socklen_t addrlen = sizeof(struct sockaddr_storage);
+  int conn_fd = accept(fd_, (struct sockaddr *)&client_addr, &addrlen);
+  fcntl(conn_fd, F_SETFD, O_NONBLOCK);
+
+  utils::LogConnectionInfoToStdout(client_addr);
+
+  Socket *conn_sock = new Socket(conn_fd, ConnSock, port_);
+  return conn_sock;
+}
+
+// ========================================================================
+// Getter and Setter
+
+int Socket::GetFd() const {
+  return fd_;
+}
+
+void Socket::SetPort(const std::string &port) {
+  port_ = port;
+}
+
+const std::string &Socket::GetPort() const {
+  return port_;
+}
+
+Socket::ESockType Socket::GetSockType() const {
+  return socktype_;
+}
+
+utils::ByteVector &Socket::GetBuffer() {
+  return buffer_;
+}
+
+void Socket::IncreaseFdReferenceCount(int fd) {
+  fd_reference_counter_[fd]++;
+}
+
+void Socket::DecreaseFdReferenceCount(int fd) {
+  assert(fd_reference_counter_[fd] > 0);
+  fd_reference_counter_[fd]--;
+}
+
+int Socket::GetFdReferenceCount(int fd) {
+  return fd_reference_counter_[fd];
+}
+
+}  // namespace server
---
+++ srcs/server/socket.hpp
@@ -0,0 +1,74 @@
+#ifndef SERVER_SOCKET_HPP_
+#define SERVER_SOCKET_HPP_
+
+#include <string>
+#include <vector>
+
+#include "http/http_request.hpp"
+#include "http/http_response.hpp"
+#include "utils/ByteVector.hpp"
+
+namespace server {
+
+// listen_fd の情報などを持たせたい｡
+// CGI でリクエスト元IPなど色々情報が必要になってくるので,
+// それらの情報をもたせるようにしたい｡
+class Socket {
+ public:
+  enum ESockType { ListenSock, ConnSock };
+
+ private:
+  int fd_;
+
+  ESockType socktype_;
+
+  // port_ は socktype_ によって意味が変わる｡
+  // ListenSock の場合は Listen しているポート番号
+  // ConnSock の場合はAcceptされたListenSockのポート番号
+  std::string port_;
+
+  std::vector<http::HttpRequest> requests_;
+  http::HttpResponse response_;
+  utils::ByteVector buffer_;
+
+  // 最後にデータ操作が行われた時間を記録する｡タイムアウト処理用｡
+
+  // fdの参照カウント.
+  // fd_reference_counter_[fd] = <reference_count>
+  static std::map<int, int> fd_reference_counter_;
+
+ public:
+  Socket(int fd, ESockType socktype, const std::string &port = "");
+  Socket(const Socket &rhs);
+  Socket &operator=(const Socket &rhs);
+  // close(fd_) はデストラクタで行われる
+  ~Socket();
+
+  // 現在の Socket に来た接続要求を accept する｡
+  // 返り値の Socket* はヒープ領域に存在しており､
+  // 解放するのは呼び出し側の責任である｡
+  Socket *AcceptNewConnection();
+
+  // ========================================================================
+  // Getter and Setter
+
+  int GetFd() const;
+
+  void SetPort(const std::string &port);
+  const std::string &GetPort() const;
+
+  ESockType GetSockType() const;
+
+  utils::ByteVector &GetBuffer();
+
+ private:
+  Socket();
+
+  void IncreaseFdReferenceCount(int fd);
+  void DecreaseFdReferenceCount(int fd);
+  int GetFdReferenceCount(int fd);
+};
+
+}  // namespace server
+
+#endif
