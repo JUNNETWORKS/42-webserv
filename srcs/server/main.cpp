4dee0361df885907f8269b2d74d979c270f799ec
Junichi Sasaki <junichi200123@gmail.com>
2022-06-12 05:48:22 +0900

    main.cpp で Epoll に listen_fd を追加する

--- srcs/server/main.cpp
+++ srcs/server/main.cpp
@@ -1,13 +1,17 @@
 #include "config/config.hpp"
+#include "result/result.hpp"
 #include "server/epoll.hpp"
 #include "server/event_loop.hpp"
 #include "server/setup.hpp"
-#include "server/socket_manager.hpp"
+#include "server/socket.hpp"
+#include "server/socket_event_handler.hpp"
 #include "server/types.hpp"
 #include "utils/error.hpp"
 #include "utils/inet_sockets.hpp"
 
 int main(int argc, char const *argv[]) {
+  using namespace result;
+
   setbuf(stdout, NULL);
   config::Config config;
   if (argc >= 2) {
@@ -23,17 +27,30 @@ int main(int argc, char const *argv[]) {
   }
   config.Print();
 
-  server::ListenFdPortMap listen_fd_port_map;
-  if (!server::OpenLilstenFds(listen_fd_port_map, config)) {
-    utils::ErrExit("OpenListenFds()");
+  Result<server::ListenFdPortMap> result = server::OpenLilstenFds(config);
+  if (result.IsErr()) {
+    utils::ErrExit("server::OpenLilstenFds()");
   }
+
+  server::ListenFdPortMap listen_fd_port_map = result.Ok();
 
   // epoll インスタンス作成
   server::Epoll epoll;
-  server::SocketManager socket_manager(epoll);
-  socket_manager.AppendListenFd(listen_fd_port_map);
 
-  server::StartEventLoop(epoll, socket_manager, config);
+  for (server::ListenFdPortMap::const_iterator it = listen_fd_port_map.begin();
+       it != listen_fd_port_map.end(); ++it) {
+    int listen_fd = it->first;
+    std::string port = it->second;
+    server::Socket *listen_sock =
+        new server::Socket(listen_fd, server::Socket::ListenSock, port, config);
+    server::FdEvent *fde = server::CreateFdEvent(
+        listen_fd, server::HandleListenSocketEvent, listen_sock);
+    if (epoll.AddFd(fde, EPOLLIN).IsErr()) {
+      utils::ErrExit("Epoll.AddFd()");
+    }
+  }
+
+  server::StartEventLoop(epoll);
 
   return 0;
 }
