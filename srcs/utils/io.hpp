36acd54f60030063186edfb6d94a8517d64901af
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2022-06-13 20:37:58 +0900

    GetFileListをResultに格納したFileのvectorを返すようにした

--- srcs/utils/io.hpp
+++ srcs/utils/io.hpp
@@ -7,13 +7,18 @@
 #include <string>
 #include <vector>
 
+#include "result/result.hpp"
+#include "utils/File.hpp"
+
 namespace utils {
+
+using namespace result;
 
 bool IsFileExist(const std::string& path);
 
 bool IsDir(const std::string& path);
 
-bool GetFileList(const std::string& path, std::vector<std::string>& vec);
+Result<std::vector<utils::File> > GetFileList(const std::string target_dir);
 
 bool PutStrFd(const std::string str, int fd);
 
