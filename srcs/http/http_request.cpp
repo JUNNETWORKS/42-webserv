e405bbd01f238f1adfc53e413e2af20060b5fa3f
Taisei Toya <tytaisei0102@gmail.com>
2022-06-17 10:57:29 +0200

    ValidateChunkFormatをValidateChunkDataFormatに変更

--- srcs/http/http_request.cpp
+++ srcs/http/http_request.cpp
@@ -331,7 +331,7 @@ HttpStatus HttpRequest::DecideBodySize() {
 namespace {
 
 // Chunk内にCRLFがあること、CRLFがチャンクの末尾についている事を検証する。
-bool ValidateChunkFormat(const Chunk &chunk, utils::ByteVector &buffer) {
+bool ValidateChunkDataFormat(const Chunk &chunk, utils::ByteVector &buffer) {
   utils::ByteVector chunk_data_bytes = utils::ByteVector(
       buffer.begin() + chunk.size_str.size() + kCrlf.size(), buffer.end());
   unsigned long crlf_pos = std::distance(chunk_data_bytes.begin(),
@@ -368,7 +368,7 @@ std::pair<Chunk::ChunkStatus, Chunk> CheckChunkReceived(
   if (buffer.size() < expect_buffer_size)
     return std::make_pair(Chunk::kWaiting, res);
 
-  if (ValidateChunkFormat(res, buffer) == false)
+  if (ValidateChunkDataFormat(res, buffer) == false)
     return std::make_pair(Chunk::kErrorBadRequest, res);
 
   return std::make_pair(Chunk::kReceived, res);
