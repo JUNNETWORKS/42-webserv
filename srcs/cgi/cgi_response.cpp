d6edb562f7f5e5db6e7483c863d7d1e57011ec59
Junichi Sasaki <junichi200123@gmail.com>
2022-06-17 12:25:43 +0900

    CgiResponseクラスの雛形を作成

--- srcs/cgi/cgi_response.cpp
+++ srcs/cgi/cgi_response.cpp
@@ -1,1 +1,23 @@
 #include "cgi/cgi_response.hpp"
+
+namespace cgi {
+
+CgiResponse::CgiResponse() {}
+
+CgiResponse::CgiResponse(const CgiResponse &rhs) {}
+
+CgiResponse &CgiResponse::operator=(const CgiResponse &rhs) {}
+
+CgiResponse::~CgiResponse() {}
+
+void CgiResponse::ParseResponse(utils::ByteVector &buffer) {}
+
+http::HttpResponse CgiResponse::ToHttpResponse() {}
+
+bool CgiResponse::IsParsed() {}
+
+// ========================================================================
+// Getter and Setter
+CgiResponse::ResponseType CgiResponse::GetResponseType() {}
+
+}  // namespace cgi
--- srcs/cgi/cgi_response.hpp
+++ srcs/cgi/cgi_response.hpp
@@ -4,14 +4,48 @@
 #include <map>
 #include <string>
 
+#include "http/http_response.hpp"
+#include "utils/ByteVector.hpp"
+
+namespace cgi {
+
 class CgiResponse {
+ public:
   enum ResponseType {
+    kParsing,
     kDocumentResponse,
     kLocalRedirect,
     kClientRedirect,
     kClientRedirectWithDocument,
     kParseError,
   };
+
+ private:
+  ResponseType response_type_;
+  std::map<std::string, std::string> headers_;
+
+  int status_;
+  std::string status_msg_;
+
+  std::string location_;
+  utils::ByteVector body_;
+
+ public:
+  CgiResponse();
+  CgiResponse(const CgiResponse &rhs);
+  CgiResponse &operator=(const CgiResponse &rhs);
+  ~CgiResponse();
+
+  void ParseResponse(utils::ByteVector &buffer);
+  http::HttpResponse ToHttpResponse();
+
+  bool IsParsed();
+
+  // ========================================================================
+  // Getter and Setter
+  ResponseType GetResponseType();
 };
+
+}  // namespace cgi
 
 #endif
