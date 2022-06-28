7b4e81128432a7e78554431e4757a174e411e599
Taisei Toya <tytaisei0102@gmail.com>
2022-06-28 23:01:03 +0200

    トレイラーのサイズ判定のため、末尾のcrlfを削除しないよう変更

--- srcs/http/http_request.cpp
+++ srcs/http/http_request.cpp
@@ -169,10 +169,12 @@ HttpRequest::ParsingPhase HttpRequest::ParseChunkedBody(
     }
 
     Chunk chunk = chunk_pair.second;
+    if (chunk.data_size == 0) {
+      buffer.erase(buffer.begin(), buffer.begin() + chunk.size_str.size());
+      return kChunkedTrailer;
+    }
     buffer.erase(buffer.begin(),
                  buffer.begin() + chunk.size_str.size() + kCrlf.size());
-    if (chunk_pair.second.data_size == 0)
-      return kParsed;
     body_.insert(body_.end(), buffer.begin(), buffer.begin() + chunk.data_size);
     buffer.erase(buffer.begin(),
                  buffer.begin() + chunk.data_size + kCrlf.size());
