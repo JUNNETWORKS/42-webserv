523055baa0c28f381430e9807f64889bbf4bad50
Junichi Sasaki <junichi200123@gmail.com>
2022-06-29 06:49:40 +0900

    HttpResponse の大まかなコードが出来た

--- srcs/http/http_response.cpp
+++ srcs/http/http_response.cpp
@@ -1,16 +1,37 @@
 #include "http/http_response.hpp"
 
+#include <unistd.h>
+
 #include <algorithm>
 #include <vector>
 
+#include "http/file_event_handler.hpp"
 #include "http/http_constants.hpp"
 #include "http/http_request.hpp"
+#include "server/epoll.hpp"
 #include "utils/io.hpp"
 #include "utils/string.hpp"
 
 namespace http {
 
-HttpResponse::HttpResponse() {}
+const std::string HttpResponse::kDefaultHttpVersion = "HTTP/1.1";
+
+HttpResponse::HttpResponse()
+    : http_version_(kDefaultHttpVersion),
+      location_(NULL),
+      epoll_(NULL),
+      file_with_events_(NULL),
+      file_fde_(NULL),
+      writtern_status_headers_count_(0) {}
+
+HttpResponse::HttpResponse(const config::LocationConf *location,
+                           server::Epoll *epoll)
+    : http_version_(kDefaultHttpVersion),
+      location_(location),
+      epoll_(epoll),
+      file_with_events_(NULL),
+      file_fde_(NULL),
+      writtern_status_headers_count_(0) {}
 
 HttpResponse::HttpResponse(const HttpResponse &rhs) {
   *this = rhs;
@@ -18,57 +39,144 @@ HttpResponse::HttpResponse(const HttpResponse &rhs) {
 
 HttpResponse &HttpResponse::operator=(const HttpResponse &rhs) {
   if (this != &rhs) {
+    http_version_ = rhs.http_version_;
     status_ = rhs.status_;
+    status_message_ = rhs.status_message_;
     headers_ = rhs.headers_;
-    status_line_ = rhs.status_line_;
-    body_ = rhs.body_;
+    body_bytes_ = rhs.body_bytes_;
   }
   return *this;
 }
 
-HttpResponse::~HttpResponse() {}
+HttpResponse::~HttpResponse() {
+  if (file_with_events_) {
+    // TODO: イベントのあったものをループで処理するときにフリー済みの領域に
+    // イベントハンドラーがアクセスする可能性がある?
+    epoll_->Unregister(file_fde_);
+    delete file_fde_;
+    delete file_with_events_;
+  }
+}
 
-void HttpResponse::AppendHeader(const std::string &header,
-                                const std::string &value) {
-  headers_[header].push_back(value);
+Result<void> HttpResponse::RegisterFile(std::string file_path) {
+  FileWithEvents *file_with_events = new FileWithEvents();
+  file_with_events->file = utils::File(file_path);
+  if (file_with_events->file.Open().IsErr()) {
+    delete file_with_events;
+    return Error();
+  }
+  file_with_events->events = 0;
+
+  server::FdEvent *fde = server::CreateFdEvent(
+      file_with_events_->file.GetFd(), HandleFileEvent, file_with_events);
+  epoll_->Register(fde);
+  epoll_->Add(fde, server::kFdeRead);
+
+  file_with_events_ = file_with_events;
+  file_fde_ = fde;
 }
 
 //========================================================================
-// setter, getter
+// Writer
 
-void HttpResponse::SetStatusLine(const std::string &status_line) {
-  status_line_ = status_line;
-}
-void HttpResponse::SetBody(const std::string &body) {
-  body_ = body;
-}
-
-const std::string &HttpResponse::GetStatusLine() const {
-  return status_line_;
-}
-const std::string &HttpResponse::GetBody() const {
-  return body_;
+Result<void> HttpResponse::Write(int fd) {
+  WriteStatusAndHeader(fd);
+  if (!body_bytes_.empty()) {
+    int write_res =
+        write(fd, body_bytes_.data(), body_bytes_.size() - written_body_count_);
+    if (write_res < 0) {
+      return Error();
+    }
+    written_body_count_ += write_res;
+  }
 }
 
-void HttpResponse::Write(int fd) const {
-  WriteStatusLine(fd);
-  WriteHeaders(fd);
-  WriteBody(fd);
+Result<void> HttpResponse::WriteStatusAndHeader(int fd) {
+  if (IsStatusAndHeadersWritingCompleted()) {
+    return;
+  }
+  if (status_and_headers_bytes_.empty()) {
+    status_and_headers_bytes_ = SerializeStatusAndHeader();
+    writtern_status_headers_count_ = 0;
+  }
+  int write_res = write(
+      fd, status_and_headers_bytes_.data() + writtern_status_headers_count_,
+      status_and_headers_bytes_.size() - writtern_status_headers_count_);
+  if (write_res < 0) {
+    return Error();
+  }
+  writtern_status_headers_count_ += write_res;
+  // この関数内のwrite後に呼び出し元でもwriteがノンブロッキングで可能かは保証されていない
 }
 
-void HttpResponse::Clear() {
-  status_ = OK;
-  headers_.clear();
-  status_line_.clear();
-  body_.clear();
+//========================================================================
+// Reponse Maker
+
+void HttpResponse::MakeResponse(server::ConnSocket *conn_sock) {}
+
+bool HttpResponse::MakeErrorResponse(const config::LocationConf *location,
+                                     const HttpRequest &request,
+                                     HttpStatus status) {
+  SetStatus(status, StatusCodes::GetMessage(status));
+
+  const std::map<http::HttpStatus, std::string> &error_pages =
+      location->GetErrorPages();
+  if (error_pages.find(status) == error_pages.end() ||
+      RegisterFile(error_pages.at(status)).IsErr()) {
+    body_bytes_ = MakeErrorResponseBody(status);
+  }
+  return true;
 }
 
-void HttpResponse::WriteStatusLine(int fd) const {
-  utils::PutStrFd(status_line_, fd);
-  utils::PutStrFd(http::kCrlf, fd);
+std::string HttpResponse::MakeErrorResponseBody(HttpStatus status) {
+  std::stringstream ss;
+  ss << status;
+  ss << " ";
+  ss << StatusCodes::GetMessage(status);
+  std::string status_with_msg = ss.str();
+
+  std::string head =
+      "<html>"
+      "<head><title>" +
+      status_with_msg + "</title></head><body>";
+  std::string body = "<h1>" + status_with_msg + "</h1>";
+  std::string tail = "</body></html>";
+  return head + body + tail;
 }
 
-void HttpResponse::WriteHeaders(int fd) const {
+//========================================================================
+// Status checker
+
+bool HttpResponse::IsReadyToWrite() {
+  // TODO: 実装する
+}
+
+bool HttpResponse::IsAllDataWritingCompleted() {
+  // TODO: 実装する
+}
+
+//========================================================================
+// Serialization
+
+utils::ByteVector HttpResponse::SerializeStatusAndHeader() {
+  utils::ByteVector bytes;
+  utils::ByteVector status_line = SerializeStatusLine();
+  bytes.insert(bytes.end(), status_line.begin(), status_line.end());
+  utils::ByteVector header_lines = SerializeHeaders();
+  bytes.insert(bytes.end(), header_lines.begin(), header_lines.end());
+  return bytes;
+}
+
+utils::ByteVector HttpResponse::SerializeStatusLine() const {
+  std::string status_line;
+  status_line += http_version_ + " ";
+  status_line += status_ + " ";
+  status_line += status_message_ + http::kCrlf;
+  return status_line;
+}
+
+utils::ByteVector HttpResponse::SerializeHeaders() const {
+  std::string header_lines;
   for (HeaderMap::const_iterator headers_it = headers_.begin();
        headers_it != headers_.end(); ++headers_it) {
     typedef HeaderMap::mapped_type HeaderValuesType;
@@ -76,92 +184,37 @@ void HttpResponse::WriteHeaders(int fd) const {
     for (HeaderValuesType::const_iterator value_it = header_values.begin();
          value_it != header_values.end(); ++value_it) {
       const std::string header = headers_it->first + ": " + *value_it;
-      utils::PutStrFd(header, fd);
-      utils::PutStrFd(http::kCrlf, fd);
+      header_lines += header + http::kCrlf;
     }
   }
-  utils::PutStrFd(http::kCrlf, fd);
-}
-
-void HttpResponse::WriteBody(int fd) const {
-  utils::PutStrFd(body_, fd);
-  utils::PutStrFd(http::kCrlf, fd);
+  return header_lines;
 }
 
 //========================================================================
-//
-void HttpResponse::MakeResponse(const config::VirtualServerConf &vserver,
-                                const HttpRequest &request) {
-  // path から LocationConf を取得
-  const config::LocationConf *location = vserver.GetLocation(request.GetPath());
-  if (!location) {
-    MakeErrorResponse(NULL, request, NOT_FOUND);
-    return;
-  }
+// Setter, Getter
 
-  if (location->GetIsCgi()) {
-    MakeCgiReponse(*location, request);
-  } else if (!location->GetRedirectUrl().empty()) {
-    MakeRedirectResponse(*location, request);
-  } else {
-    MakeFileResponse(*location, request);
-  }
+void HttpResponse::SetHttpVersion(const std::string &http_version) {
+  http_version_ = http_version;
 }
 
-bool HttpResponse::MakeErrorResponse(const config::LocationConf *location,
-                                     const HttpRequest &request,
-                                     HttpStatus status) {
-  // TODO: 実装する
-  (void)location;
-  (void)request;
-  (void)status;
-  SetStatusLine("HTTP/1.1 404 Not Found");
-  // TODO: location にエラーページが設定されていればそれをBodyにセットする
-  return true;
+void HttpResponse::SetStatus(HttpStatus status) {
+  status_ = status;
+  status_message_ = StatusCodes::GetMessage(status);
 }
 
-bool HttpResponse::MakeFileResponse(const config::LocationConf &location,
-                                    const HttpRequest &request) {
-  std::string file_data;
-
-  const std::string &abs_file_path = location.GetRootDir() + request.GetPath();
-  if (!utils::IsFileExist(abs_file_path)) {
-    MakeErrorResponse(&location, request, NOT_FOUND);
-    return false;
-  }
-
-  if (utils::IsDir(abs_file_path)) {
-    SetBody(MakeAutoIndex(location.GetRootDir(), request.GetPath()));
-    SetStatusLine("HTTP/1.1 200 OK");
-    AppendHeader("Content-Type", "text/html");
-    return true;
-  }
-
-  if (!utils::ReadFile(abs_file_path, file_data)) {
-    MakeErrorResponse(&location, request, FORBIDDEN);
-    return false;
-  }
-
-  SetStatusLine("HTTP/1.1 200 OK");
-  SetBody(file_data);
-  AppendHeader("Content-Type", "text/plain");
-  return true;
+void HttpResponse::SetStatus(HttpStatus status,
+                             const std::string &status_message) {
+  status_ = status;
+  status_message_ = status_message;
 }
 
-bool HttpResponse::MakeRedirectResponse(const config::LocationConf &location,
-                                        const HttpRequest &request) {
-  // TODO: 実装する
-  (void)location;
-  (void)request;
-  return true;
+void HttpResponse::SetStatusMessage(const std::string &status_message) {
+  status_message_ = status_message;
 }
 
-bool HttpResponse::MakeCgiReponse(const config::LocationConf &location,
-                                  const HttpRequest &request) {
-  // TODO: 実装する
-  (void)location;
-  (void)request;
-  return true;
+void HttpResponse::AppendHeader(const std::string &header,
+                                const std::string &value) {
+  headers_[header].push_back(value);
 }
 
 }  // namespace http
--- srcs/http/http_response.hpp
+++ srcs/http/http_response.hpp
@@ -9,41 +9,87 @@
 #include "http/http_request.hpp"
 #include "http/http_status.hpp"
 #include "http/types.hpp"
+#include "server/epoll.hpp"
+#include "server/socket.hpp"
+#include "utils/File.hpp"
 
 namespace http {
+
+struct FileWithEvents {
+  utils::File file;
+  // どのイベント(kFdeEvents)が可能か
+  unsigned int events;
+  // ファイルから読み取れるときに読み込んでおく
+  utils::ByteVector buffer;
+};
 
 using namespace result;
 
 class HttpResponse {
- private:
+ protected:
+  static const std::string kDefaultHttpVersion;
+
+  const config::LocationConf *location_;
+  // File Response の場合は file_fd を監視したい
+  server::Epoll *epoll_;
+
+  // Status Line
+  std::string http_version_;
   HttpStatus status_;
+  std::string status_message_;
+
+  // Header
   HeaderMap headers_;
-  std::string status_line_;
-  std::string body_;
+
+  // Status Line と Headers のバイト列と書き込んだバイト数
+  utils::ByteVector status_and_headers_bytes_;
+  unsigned long writtern_status_headers_count_;
+
+  // Body のバイト列と書き込んだバイト数
+  utils::ByteVector body_bytes_;
+  unsigned long written_body_count_;
+
+  // File
+  // 全てのレスポンスクラスはファイルを返せる必要がある｡
+  // なぜならエラー時にファイルを扱う可能性があるからである｡
+  FileWithEvents *file_with_events_;
+  // レスポンス完了後､Epollの監視を解除するために必要
+  server::FdEvent *file_fde_;
 
  public:
   HttpResponse();
-
+  HttpResponse(const config::LocationConf *location, server::Epoll *epoll);
   HttpResponse(const HttpResponse &rhs);
-
   ~HttpResponse();
 
-  void SetStatusLine(const std::string &status_line);
-  void AppendHeader(const std::string &header, const std::string &value);
-  void SetBody(const std::string &body);
+  virtual void MakeResponse(server::ConnSocket *conn_sock);
 
-  const std::string &GetStatusLine() const;
-  const std::string &GetBody() const;
-
-  void MakeResponse(const config::VirtualServerConf &vserver,
-                    const HttpRequest &request);
   bool MakeErrorResponse(const config::LocationConf *location,
                          const HttpRequest &request, HttpStatus status);
 
-  void Write(int fd) const;
+  virtual Result<void> Write(int fd);
 
-  // すべてのメンバー変数を初期化する
-  void Clear();
+  // データ書き込みが可能か
+  virtual bool IsReadyToWrite();
+
+  // すべてのデータの write が完了したか
+  virtual bool IsAllDataWritingCompleted();
+
+ protected:
+  // ファイルをopenし､Epollで監視する
+  Result<void> RegisterFile(std::string file_path);
+
+  Result<void> WriteStatusAndHeader(int fd);
+
+  bool IsStatusAndHeadersWritingCompleted();
+
+  // ========================================================================
+  // Getter and Setter
+  void SetHttpVersion(const std::string &http_version);
+  void SetStatus(HttpStatus status);
+  void SetStatus(HttpStatus status, const std::string &status_message);
+  void SetStatusMessage(const std::string &status_message);
+  void AppendHeader(const std::string &header, const std::string &value);
 
  private:
   HttpResponse &operator=(const HttpResponse &rhs);
@@ -51,13 +97,16 @@ class HttpResponse {
   void WriteHeaders(int fd) const;
   void WriteBody(int fd) const;
 
+  // StatusLine と Headers をバイト列にする
+  utils::ByteVector SerializeStatusAndHeader();
+  utils::ByteVector SerializeStatusLine() const;
+  utils::ByteVector SerializeHeaders() const;
+
+  std::string MakeErrorResponseBody(HttpStatus status);
+
   // Making response
   bool MakeFileResponse(const config::LocationConf &location,
                         const HttpRequest &request);
-  bool MakeRedirectResponse(const config::LocationConf &location,
-                            const HttpRequest &request);
-  bool MakeCgiReponse(const config::LocationConf &location,
-                      const HttpRequest &request);
 
   std::string MakeAutoIndex(const std::string &root_path,
                             const std::string &relative_path) const;
