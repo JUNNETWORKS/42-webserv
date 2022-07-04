bc3b6132da564b770ff82d44795e0faf2c5da900
Junichi Sasaki <junichi200123@gmail.com>
2022-07-04 21:53:42 +0900

    LocalRedirect 以外の MakeResponse を作成

--- srcs/http/http_cgi_response.cpp
+++ srcs/http/http_cgi_response.cpp
@@ -4,32 +4,125 @@ namespace http {
 
 HttpCgiResponse::HttpCgiResponse(const config::LocationConf *location,
                                  server::Epoll *epoll)
-    : HttpResponse(location, epoll), cgi_process_(location, epoll) {}
+    : HttpResponse(location, epoll),
+      cgi_process_(new cgi::CgiProcess(location, epoll)) {}
 
-HttpCgiResponse::~HttpCgiResponse() {}
+HttpCgiResponse::~HttpCgiResponse() {
+  if (cgi_process_->IsUnregistered()) {
+    delete cgi_process_;
+  } else {
+    cgi_process_->SetIsUnregistered(true);
+  }
+}
 
 void HttpCgiResponse::MakeResponse(server::ConnSocket *conn_sock) {
-  http::HttpRequest &request = conn_sock->GetRequests().front();
-
-  if (!cgi_process_.IsCgiExecuted()) {
-    if (cgi_process_.RunCgi(request).IsErr()) {
+  if (!cgi_process_->IsCgiExecuted()) {
+    http::HttpRequest &request = conn_sock->GetRequests().front();
+    if (cgi_process_->RunCgi(request).IsErr()) {
       MakeErrorResponse(request, SERVER_ERROR);
       return;
+    }
+  } else {
+    // CGIプロセスのレスポンスタイプを確認する
+    //   - DocumentResponse        -> HttpResponseのメンバー変数を設定する
+    //   - LocalRedirect           -> ConnSock.requestsからrequestを取り出し､
+    //                                書き換えたrequestを挿入
+    //   - ClientRedirect(WithDoc) -> HttpResponseのメンバー変数を設定する
+    cgi::CgiResponse::ResponseType type =
+        cgi_process_->GetCgiResponse()->GetResponseType();
+    if (type == cgi::CgiResponse::kParseError) {
+      http::HttpRequest &request = conn_sock->GetRequests().front();
+      MakeErrorResponse(request, SERVER_ERROR);
+    } else if (type == cgi::CgiResponse::kDocumentResponse) {
+      MakeDocumentResponse(conn_sock);
+    } else if (type == cgi::CgiResponse::kLocalRedirect) {
+      MakeLocalRedirectResponse(conn_sock);
+    } else if (type == cgi::CgiResponse::kClientRedirect ||
+               type == cgi::CgiResponse::kClientRedirectWithDocument) {
+      MakeClientRedirectResponse(conn_sock);
+    } else {
+      // Not Identified
     }
   }
 }
 
-Result<void> HttpCgiResponse::Write(int fd);
-
 // データ書き込みが可能か
 bool HttpCgiResponse::IsReadyToWrite() {
-  cgi::CgiResponse *cgi_response = cgi_process_.GetCgiResponse();
-  return cgi_process_.IsCgiExecuted() &&
+  cgi::CgiResponse *cgi_response = cgi_process_->GetCgiResponse();
+
+  // Trueの場合
+  // - MakeErrorResponseが呼ばれている
+  // - CGIスクリプトが正しく実行されている
+  //   && CGIスクリプト出力タイプが決定している
+  //   && CGIスクリプトが LocalRedirect ではない
+  //   && (StatusとHeaderがまだ書き込まれていない
+  //       || ボディにデータが追加されている)
+  return cgi_process_->IsCgiExecuted() &&
          cgi_response->GetResponseType() != cgi::CgiResponse::kNotIdentified &&
          !cgi_response->GetBody().empty();
 }
 
 // すべてのデータの write が完了したか
 bool HttpCgiResponse::IsAllDataWritingCompleted();
+
+void HttpCgiResponse::MakeDocumentResponse(server::ConnSocket *conn_sock) {
+  SetStatusFromCgiResponse();
+  SetHeadersFromCgiResponse();
+}
+
+void HttpCgiResponse::MakeLocalRedirectResponse(server::ConnSocket *conn_sock) {
+
+}
+
+void HttpCgiResponse::MakeClientRedirectResponse(
+    server::ConnSocket *conn_sock) {
+  cgi::CgiResponse *cgi_response = cgi_process_->GetCgiResponse();
+  if (cgi_response->GetHeader("Status").IsOk()) {
+    SetStatusFromCgiResponse();
+  } else {
+    SetStatus(FOUND);
+  }
+  SetHeadersFromCgiResponse();
+}
+
+Result<void> HttpCgiResponse::Write(int fd) {
+  cgi::CgiResponse *cgi_response = cgi_process_->GetCgiResponse();
+}
+
+void HttpCgiResponse::SetStatusFromCgiResponse() {
+  cgi::CgiResponse *cgi_response = cgi_process_->GetCgiResponse();
+
+  Result<std::string> status_result = cgi_response->GetHeader("Status");
+  if (status_result.IsErr()) {
+    // TODO: エラーを返すべき?
+    return;
+  }
+
+  std::string status_value = status_result.Ok();
+  std::size_t space_pos = status_value.find(" ");
+  if (space_pos == std::string::npos) {
+    return;
+  }
+  std::string status_str = status_value.substr(0, space_pos);
+  Result<unsigned long> status = utils::Stoul(status_str);
+  std::string status_msg = status_value.substr(space_pos);
+  if (status.IsErr() || !StatusCodes::IsHttpStatus(status.Ok()) ||
+      status_msg.empty()) {
+    return;
+  }
+  SetStatus(static_cast<HttpStatus>(status.Ok()), status_msg);
+}
+
+void HttpCgiResponse::SetHeadersFromCgiResponse() {
+  cgi::CgiResponse *cgi_response = cgi_process_->GetCgiResponse();
+  const cgi::CgiResponse::HeaderVecType &header_vec =
+      cgi_response->GetHeaders();
+  for (cgi::CgiResponse::HeaderVecType::const_iterator it = header_vec.begin();
+       it != header_vec.end(); ++it) {
+    if (it->first != "STATUS") {
+      SetHeader(it->first, it->second);
+    }
+  }
+}
 
 }  // namespace http
--- srcs/http/http_cgi_response.hpp
+++ srcs/http/http_cgi_response.hpp
@@ -7,30 +7,9 @@
 
 namespace http {
 
-struct CgiBuffer {
-  // 1回のreadで何バイト読み取るか
-  static const unsigned long kBytesPerRead = 1024;  // 1KB
-  // bufferが保持する最大サイズ｡
-  // 1リクエストがメモリを大量に使わないようにするために存在する
-  static const unsigned long kMaxBufferSize = 1024 * 50;  // 50KB
-
-  int file_fd;
-
-  // Epoll events
-  unsigned int events;
-  bool is_eof;
-
-  // このフラグがfalseのときにはdeleteせずにこのフラグをtrueにするだけ｡
-  // HttpResponse と EpollEventHandler で double-free しないようにするため
-  bool is_unregistered;
-
-  // ファイルから読み取れるときに読み込んでおく
-  utils::ByteVector buffer;
-};
-
 class HttpCgiResponse : public HttpResponse {
  private:
-  cgi::CgiProcess cgi_process_;
+  cgi::CgiProcess *cgi_process_;
 
  public:
   HttpCgiResponse(const config::LocationConf *location, server::Epoll *epoll);
@@ -44,7 +23,16 @@ class HttpCgiResponse : public HttpResponse {
 
   // すべてのデータの write が完了したか
   virtual bool IsAllDataWritingCompleted();
+
+ private:
+  void MakeDocumentResponse(server::ConnSocket *conn_sock);
+  void MakeLocalRedirectResponse(server::ConnSocket *conn_sock);
+  void MakeClientRedirectResponse(server::ConnSocket *conn_sock);
+
+  void SetStatusFromCgiResponse();
+  void SetHeadersFromCgiResponse();
 };
+
 }  // namespace http
 
 #endif
