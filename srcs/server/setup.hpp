ebf0aa668bc9ee684b662f01ccc54be46a609f45
Junichi Sasaki <junichi200123@gmail.com>
2022-07-15 23:02:47 +0900

    RegisterListenSockets() で Listenソケットの作成と監視登録全てを行う

--- srcs/server/setup.hpp
+++ srcs/server/setup.hpp
@@ -13,16 +13,5 @@ using namespace result;
 
 Result<void> RegisterListenSockets(Epoll &epoll, const config::Config &config);
 
-// config内のバーチャルサーバの情報を元に必要なソケットをオープンし､リッスン状態にする｡
-// 返り値は map[<listen_fd>] = <port> の形のmap
-Result<ListenFdPortMap> OpenLilstenFds(const config::Config &config);
-
-// fdsのすべての要素に対してcloseシステムコールを実行する｡
-void CloseAllFds(const ListenFdPortMap &listen_fd_port_map);
-
-// listen_fd_port_map を Epoll で監視するようにする
-void AddListenFds2Epoll(Epoll &epoll, const config::Config &config,
-                        const ListenFdPortMap &listen_fd_port_map);
-
 }  // namespace server
 #endif
