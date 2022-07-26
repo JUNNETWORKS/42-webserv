1dbb5633f98759a44dff0b16d192ff44bbe0e810
Junichi Sasaki <junichi200123@gmail.com>
2022-07-27 02:40:58 +0900

    server4 はディレクトリが存在しない｡setup_review_env.

--- review/setup_review_env.sh
+++ review/setup_review_env.sh
@@ -17,7 +17,6 @@ cp -r error_pages /var/webserv/error_pages
 cp -r server1 /var/webserv/server1
 cp -r server2 /var/webserv/server2
 cp -r server3 /var/webserv/server3
-cp -r server4 /var/webserv/server4
 
 if grep -q "127.0.0.1 webserv.com" /etc/hosts; then
   echo "127.0.0.1 webserv.com" >> /etc/hosts
